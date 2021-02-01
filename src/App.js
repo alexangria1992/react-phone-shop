199431d454f44950a6107d01ba6fcf25e2aec6c2
alexangria1992 <alex040892@gmail.com>
2021-02-01 18:18:08 +1300

    Set up React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
+import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from './components/Default';
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <h3>Hello from app</h3>
+        <Navbar/>
+        <Switch>
+        <Route path="/details" component={Details}/>
+          <Route path="/cart" component={Cart}/>
+          <Route exact path="/" component={ProductList}/>
+  
+          <Route component={Default}/>
+
+        </Switch>
       </React.Fragment>
     )
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from Navbar</h3>
+            </div>
+        )
+    }
+}
